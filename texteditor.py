a25fa5c949a63cc918a13df22e325a2244c7f4bc
meghanadhmandepudi <meghanadhmandepudi3@gmail.com>
2024-12-30 00:44:16 +0530

    added open file and new file

--- texteditor.py
+++ texteditor.py
@@ -16,3 +16,19 @@ def open_file():
                 text_area.insert(tk.END, content)
         except Exception as e:
             messagebox.showerror("Error",f"Could not open file: {e}")
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("Text Files","*.txt"), ("All Files", "*.*")])
+    if file_path:
+        try:
+            with open(file_path,"w") as file:
+                content = text_area.get(1.0, tk.END)
+                File.write(content.strip())
+                messagebox.showinfo("success", "file saved successfully!")
+        except Exception as e:
+            messagebox.showerror("error",f"could not save file: {e}")
+def toggle_dark_mode():
+    current_bg = text_area.cget("background")
+    if current_bg == "white":
+        text_area.config(background="black", foreground="white", insertbackground="white")
+    else:
+        text_area.config(background="white", foreground="black", insertbackground="black")
